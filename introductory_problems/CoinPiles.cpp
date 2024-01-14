4fc11bd35b0de190ae754fb4d785c5d96abbed80
poemsforaphrodite <pushpendersolanki_co21a6_23@dtu.ac.in>
2024-01-14 22:31:32 +0530

    Add CoinPiles.cpp implementation

---
+++ introductory_problems/CoinPiles.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int n;
+    cin>>n;
+    while(n--){
+        long long int a,b;
+        cin>>a>>b;
+        if(b<a)swap(a,b);
+        if(a==b && a%3 ==0){
+            cout<<"YES"<<endl;
+        }else if(a == b/2 && b%a == 0){
+            cout<<"YES"<<endl;
+        }else if(a == (b+3)/2&&b%a ==0){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }   
+    return 0;
+}
